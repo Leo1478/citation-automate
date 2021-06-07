22807ef6b90a18f792cf7357173c0004355bb8fb
Leo1478 <leozeng1478@gmail.com>
2021-06-06 21:43:23 -0400

    code works now,but cringe ass website has a adblocker popup that stops you from citing more that 2 at a time

--- citation automate/citation automate 2.py
+++ citation automate/citation automate 2.py
@@ -5,6 +5,8 @@ import json
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 
@@ -17,34 +19,24 @@ class TestCitation():
         self.driver.quit()
 
     def repeat(self):
-        self.driver.find_element(By.CSS_SELECTOR, "*[data-test=\"create-citations-btn\"]").click()
         self.driver.implicitly_wait(10)
         self.driver.find_element(By.CSS_SELECTOR, "*[data-test=\"website-button\"]").click()
         self.driver.find_element(By.CSS_SELECTOR, "*[data-test=\"citation-search-input\"]").click()
-        self.driver.find_element(By.CSS_SELECTOR, "html").click()
-        self.driver.find_element(By.CSS_SELECTOR, "*[data-test=\"citation-search-input\"]").click()
         self.driver.find_element(By.CSS_SELECTOR, "*[data-test=\"citation-search-input\"]").send_keys(
-            "https://www.citationmachine.net/")
+            link)
         self.driver.find_element(By.CSS_SELECTOR, ".sc-fzoXWK > .sc-fzoMdx").click()
-        element = self.driver.find_element(By.CSS_SELECTOR, ".sc-fzoMdx")
-        actions = ActionChains(self.driver)
+        self.driver.find_element(By.CSS_SELECTOR, "*[data-test=\"cite-button\"]").click()
         self.driver.implicitly_wait(10)
-        actions.move_to_element(element).perform()
-        element = self.driver.find_element(By.CSS_SELECTOR, "body")
-        actions = ActionChains(self.driver)
-        actions.move_to_element(element, 0, 0).perform()
-        self.driver.find_element(By.CSS_SELECTOR, ".sc-fzoMdx > .styled__ResultButtonText-jhqr36-14").click()
         self.driver.find_element(By.CSS_SELECTOR, "*[data-test=\"submit-eval\"]").click()
-        element = self.driver.find_element(By.CSS_SELECTOR, "*[data-test=\"submit-eval\"]")
-        actions = ActionChains(self.driver)
-        actions.move_to_element(element).perform()
         self.driver.find_element(By.CSS_SELECTOR, "*[data-test=\"form-next-button\"]").click()
-        element = self.driver.find_element(By.CSS_SELECTOR, ".jEGbDM")
-        actions = ActionChains(self.driver)
-        actions.move_to_element(element).perform()
-        self.driver.find_element(By.CSS_SELECTOR, ".button__SpecialButton-sc-1lt38zg-1 > .sc-fzoMdx").click()
+        self.driver.find_element(By.CSS_SELECTOR, "*[data-test=\"create-new-cite-btn\"]").click()
+
     def start(self):
         self.driver.get("https://www.citationmachine.net/")
+        self.driver.set_window_size(1294, 1407)
+        self.driver.implicitly_wait(10)
+        self.driver.find_element(By.CSS_SELECTOR, ".styled__CardContainer-sc-1468au2-0:nth-child(1) .sc-fzoMdx").click()
+        self.driver.implicitly_wait(10)
 
 
 file1 = open("list_of_webcites.txt","r")
